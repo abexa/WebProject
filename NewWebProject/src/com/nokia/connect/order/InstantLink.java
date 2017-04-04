fc43bcb06971bfd82b1a6d927bfca67b1472f563
oliviuspis <pisollie94@gmail.com>
2017-04-04 16:31:01 +0300

    get driverPath from config file

--- NewWebProject/src/com/nokia/connect/order/InstantLink.java
+++ NewWebProject/src/com/nokia/connect/order/InstantLink.java
@@ -1,23 +1,29 @@
 package com.nokia.connect.order;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 public class InstantLink  extends Actions {
 	
 	private String ilLink = "http://ec2-52-63-80-249.ap-southeast-2.compute.amazonaws.com:44080/sas5";
-	private String driverPath = "C:\\Users\\opis\\Documents\\CHORUS\\Selenium\\chromedriver.exe";
 	private String logInButton = "btnauthenticate";
 	private String searchOrderButton = "";
+	private Properties props;	
 	
 	public InstantLink() throws FileNotFoundException, IOException {
 		super();
+		props = new Properties();
+		props.load(new FileInputStream("files/config.properties"));
 		// TODO Auto-generated constructor stub
 	}
 	
 
 	public void ilActions(){ 
-	
+		
+		String driverPath = props.getProperty("driverPath");
+		
 		openWeb(driverPath);
 		openWebPage(ilLink);
 		login();
